64436a039ea3e35a0402b2578a7103b5ae08f3e3
Loïck LEPREVOST <loick.leprevost@gmail.com>
2022-01-23 17:31:03 +0100

    footer stick bot

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Layout from "../components/layout/Layout";
 import ContactForm from "../components/common/ContactForm";
+import styles from "./contact.module.scss";
 
 const Contact = () => {
     return <Layout title="Portfolio">
         {/* SECTION CONTACT */}
-        <ContactForm />
+        <div className={styles.containerContact}>
+        <ContactForm /></div>
     </Layout>
 }
 
